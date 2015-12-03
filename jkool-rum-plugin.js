02b879753baee130492bd61063c5f54bf1594875
Cathy Bernardone <cbernardone@nastel.com>
2015-12-03 12:38:39 -0500

    More progress.

--- jkool-rum-plugin.js
+++ jkool-rum-plugin.js
@@ -308,39 +308,12 @@ if (('performance' in window) & ('timing' in window.performance)
 			},
 		});
 
-		var perfEntries = window.performance.getEntriesByType("mark");
-		alert("in mark");
-		alert(perfEntries);
-		for (var i = 0; i < perfEntries.length; i++) {
-			alert(perfEntries[i].entryName);
-			// var myJSONData =
-			// '{"tracking-id":"51191015-1783-4f63-b91e-7e17e8e33333","status":"END","time-usec":"1444679553236000","operation":"Performance","source_fqn":"APPL=WebOrders#SERVER=WebServer100#NETADDR=11.0.0.2#DATACENTER=DC1#GEOADDR=New
-			// York, NY"}';
-			// $.ajax({
-			// type: 'POST',
-			// url: 'http://localhost:6580/JESL/activity',
-			// data: myJSONData,
-			// dataType: 'text',
-			// headers:{'token':'775d182c-fb54-4eb1-be58-1153ce2d7865'},
-			// success: function(data) {
-			// alert(data);
-			// },
-			// error: function(jqXHR, textStatus, errorThrown) {
-			// alert("HelloStatus: " + textStatus); alert("HelloError: " +
-			// errorThrown);
-			// },
-			// statusCode: {
-			// 202: function() {
-			// alert( "Request accepted.")}}
-			// });
-		}
-
 	}
 
 	)
 
-	// onLoad (To prevent zero from being reported, need to put it in a timeout
-	// function)
+	// onLoad & Ajax
+	// (To prevent zero from being reported, need to put it in a timeout function)
 	setTimeout(function() {
 
 		var timings = window.performance.timing;
