85e7f41e4485b670cde0d46d4ac6395ee2bf6a5e
cbernardone <cbernardone@computer70.nastel1.main>
2016-07-14 10:44:12 -0400

    added copyright, licensing to source files.

--- jkool-rum-plugin.php
+++ jkool-rum-plugin.php
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2014 jKool, LLC. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * jKool, LLC. ("Confidential Information").  You shall not disclose
+ * such Confidential Information and shall use it only in accordance with
+ * the terms of the license agreement you entered into with jKool, LLC.
+ *
+ * JKOOL MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+ * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, OR NON-INFRINGEMENT. JKOOL SHALL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
+ * THIS SOFTWARE OR ITS DERIVATIVES.
+ *
+ * CopyrightVersion 1.0
+ *
+ */
+
 <?php
 function createGuid(){
     if (function_exists('com_create_guid')){
